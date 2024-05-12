e811a3281cdc86839318e1f2a3d258fcd0f87774
Ng Yat Yan <ng.yatyan@gmail.com>
2024-05-12 16:29:45 +0800

    Add Springboot Support

---
+++ readme.md
@@ -0,0 +1,5 @@
+Please add this vm parameter before running:
+
+```
+--module-path "/path/to/javafx/javafx-sdk-17.0.10" --add-modules javafx.controls,javafx.fxml
+```
---
+++ src/main/java/com/example/javafx/springboot/app/Boot.java
@@ -0,0 +1,9 @@
+package com.example.javafx.springboot.app;
+
+public class Boot {
+
+	public static void main(String[] args) {
+		JavaFxApplicationSupport.boot(args);
+	}
+
+}
